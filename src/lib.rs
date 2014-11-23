9cda8e9484b54d215f4a0549913da94eb2eb77cd
Dabo Ross <daboross@daboross.net>
2014-11-23 02:54:22 -0800

    It took a long time (and quite a bit of hackery), but I finally got dynamically registering for commands working :D

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,23 @@
 #![feature(globs)]
+#![feature(phase)]
+#[phase(plugin)]
+
+extern crate regex_macros;
+extern crate regex;
 
 use std::io::*;
 use std::sync::atomic::*;
-use std::sync::Arc;
+use std::sync::{Arc, RWLock};
+use std::ascii::AsciiExt;
+use regex::Regex;
+use std::collections::HashMap;
 
 pub struct IrcClient {
     socket: TcpStream,
     reader: Option<BufferedReader<TcpStream>>,
-    reader_created: Arc<AtomicBool>
+    reader_created: Arc<AtomicBool>,
+    pub permitted: Regex,
+    listeners: Arc<RWLock<HashMap<String, Vec<|&mut IrcClient, &str, &[&str], Option<&str>|:'static>>>>
 }
 
 impl IrcClient {
@@ -15,7 +25,9 @@ impl IrcClient {
         return IrcClient {
             socket: TcpStream::connect(addr).unwrap(),
             reader: None,
-            reader_created: Arc::new(AtomicBool::new(false))
+            reader_created: Arc::new(AtomicBool::new(false)),
+            permitted: regex!(r"^Dabo[^!]*![^@]*@me.dabo.guru$"),
+            listeners: Arc::new(RWLock::new(HashMap::new()))
         }
     }
 
@@ -26,16 +38,75 @@ impl IrcClient {
         self.socket.flush().ok().expect("Failed to flush stream");
     }
 
-    pub fn init_reader(&mut self) {
+    pub fn start_receiving(&self) {
+        let reader = self.clone();
+        reader.spawn_reading_thread();
+    }
+
+    pub fn add_listener(&mut self, command: &str, f: |&mut IrcClient, &str, &[&str], Option<&str>|:'static) {
+        let command_string = command.into_string().to_ascii_lower();
+        let mut listener_map = self.listeners.write();
+        {
+            // I can't use a match here because then I would be borrowing listener_map mutably twice?
+            // Once for the match statement, and a second time inside the None branch
+            if listener_map.contains_key(&command_string) {
+                listener_map.get_mut(&command_string).expect("Wat").push(f);
+            } else {
+                listener_map.insert(command_string, vec!(f));
+            }
+        }
+        listener_map.downgrade();
+    }
+
+    fn init_reader(&mut self) {
         if self.reader_created.swap(true, Ordering::Relaxed) {
             panic!("Reader already created");
         }
         self.reader = Some(BufferedReader::new(self.socket.clone()));
     }
 
-    pub fn read_line(&mut self) -> Result<String, IoError> {
+    fn read_line(&mut self) -> Result<String, IoError> {
         let reader = self.reader.as_mut().unwrap();
-        return reader.read_line()
+        let whole_line = try!(reader.read_line());
+        return Ok(whole_line.trim_right().into_string())
+    }
+
+    fn spawn_reading_thread(mut self) {
+        spawn(proc() {
+            self.init_reader();
+            loop {
+                let input = match self.read_line() {
+                    Ok(v) => v,
+                    Err(e) => {
+                        println!("Error: {}", e);
+                        break;
+                    }
+                };
+                let message_split: Vec<&str> = input.split(' ').collect();
+                let (command, args, possible_mask) = if message_split[0].starts_with(":") {
+                    (message_split[1], message_split.slice_from(2), Some(message_split[0].slice_from(1)))
+                } else {
+                    (message_split[0], message_split.slice_from(1), None)
+                };
+                match possible_mask {
+                    Some(v) => println!("Received {} from {}: {}", command, v, args.connect(" ")),
+                    None => println!("Received {}: {}", command, args.connect(" "))
+                }
+                let shared_self = &mut self.clone();
+                let mut listener_map = self.listeners.write();
+                {
+                    let mut command_listeners = match listener_map.get_mut(&command.to_ascii_lower()) {
+                        Some(v) => v,
+                        None => continue
+                    };
+
+                    for listener in command_listeners.iter_mut() {
+                        (*listener)(shared_self, command, args, possible_mask);
+                    }
+                }
+                listener_map.downgrade();
+            }
+        });
     }
 }
 
@@ -44,13 +115,17 @@ impl Clone for IrcClient {
         return IrcClient {
             socket: self.socket.clone(),
             reader: None,
-            reader_created: self.reader_created.clone()
+            reader_created: self.reader_created.clone(),
+            permitted: self.permitted.clone(),
+            listeners: self.listeners.clone()
         }
     }
 
     fn clone_from(&mut self, source: &IrcClient) {
         self.socket = source.socket.clone();
         self.reader = None;
-        self.reader_created = self.reader_created.clone();
+        self.reader_created = source.reader_created.clone();
+        self.permitted = source.permitted.clone();
+        self.listeners = source.listeners.clone();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,42 +5,23 @@ use std::ascii::AsciiExt;
 
 fn main() {
     let mut client = IrcClient::new("irc.spi.gt:6667");
-    let mut reader = client.clone();
-
-    client.send("NICK TestBot");
+    client.send("NICK bot");
     client.send("USER rust 0 * :Test");
-
-    spawn(proc() {
-        reader.init_reader();
-        loop {
-            let input = match reader.read_line() {
-                Ok(v) => v,
-                Err(e) => {
-                    println!("Error: {}", e);
-                    break;
-                }
-            };
-            println!("Received: {}", input.trim());
-            let split: Vec<&str> = input.split(' ').collect();
-            let (command, args) = if split[0].starts_with(":") {
-                (split[1], split.slice_from(2))
-            } else {
-                (split[0], split.slice_from(1))
-            };
-            if command == "PING" {
-                reader.send(format!("PONG {}", args[0]).as_slice());
-            } else if command == "004" {
-                reader.send("JOIN #testbot");
-                reader.send("PRIVMSG #testbot :Hello!");
-            } else if command == "PRIVMSG" {
-                if args[args.len() - 1].trim().eq_ignore_ascii_case(":quit") {
-                    reader.send("QUIT :Testing.");
-                    break;
-                } else {
-                    println!("{}", args[args.len() - 1]);
-                    reader.send(format!("PRIVMSG {}", args.connect(" ")).as_slice());
-                }
-            }
+    client.start_receiving();
+    client.add_listener("ping", |client: &mut IrcClient, cmd: &str, args: &[&str], mask: Option<&str>| {
+        client.send(format!("PONG {}", args[0]).as_slice());
+    });
+    client.add_listener("004", |client: &mut IrcClient, cmd: &str, args: &[&str], mask: Option<&str>| {
+        client.send("JOIN #bot");
+    });
+    client.add_listener("privmsg", |client: &mut IrcClient, cmd: &str, args: &[&str], possible_mask: Option<&str>| {
+        let mask = possible_mask.expect("PRIVMSG received without sender mask");
+        if args[1].eq_ignore_ascii_case(":quit") && client.permitted.is_match(mask) {
+            client.send("QUIT :Testing.");
+        } else if args[1].eq_ignore_ascii_case(":raw") && client.permitted.is_match(mask) {
+            client.send(args.slice_from(2).connect(" ").as_slice())
+        } else {
+            client.send(format!("PRIVMSG {}", args.connect(" ")).as_slice());
         }
     });
 }
