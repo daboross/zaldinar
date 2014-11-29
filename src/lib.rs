351864ec65aaf40c74384f71e2f2a16c84e048a7
Dabo Ross <daboross@daboross.net>
2014-11-29 13:01:15 -0800

    Show parsing errors a lot more readably

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ use serialize::json;
 use regex::Regex;
 use std::error::Error;
 use serialize::json::DecoderError;
+use serialize::json::ParserError;
 
 pub enum InitializationError {
     Io(IoError),
@@ -22,6 +23,10 @@ pub enum InitializationError {
 impl InitializationError {
     fn new(detail: &str) -> InitializationError {
         InitializationError::Other(detail.to_string())
+    }
+
+    fn from_string(detail: String) -> InitializationError {
+        InitializationError::Other(detail)
     }
 }
 
@@ -391,7 +396,19 @@ pub struct ClientConfiguration {
 
 pub fn load_config_from_file(path: &Path) -> Result<ClientConfiguration, InitializationError> {
     let config_contents = try!(File::open(path).read_to_string());
-    let client_config = try!(json::decode::<ClientConfiguration>(config_contents.as_slice()));
+    let client_config = try!(match json::decode::<ClientConfiguration>(config_contents.as_slice()) {
+        Ok(v) => Ok(v),
+        Err(e) => {
+            match e {
+                DecoderError::ParseError(parse_error) => match parse_error {
+                    ParserError::SyntaxError(error_code, line, col) => return Err(InitializationError::from_string(format!("Syntax error ({}) on line {} column {} in {}", error_code, line, col, path.display()))),
+                    ParserError::IoError(kind, desc) => return Err(InitializationError::Io(IoError{ kind: kind, desc: desc, detail: None}))
+                },
+                DecoderError::MissingFieldError(s) => return Err(InitializationError::from_string(format!("Field {} not found in {}", s.as_slice(), path.display()))),
+                _ => Err(e)
+            }
+        }
+    });
 
     return Ok(client_config)
 }
