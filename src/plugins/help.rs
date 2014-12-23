0abe5720acaea8af7bfc725e4794394af949491f
Dabo Ross <daboross@daboross.net>
2014-12-23 02:34:56 -0800

    Simplify statement a bit, because it's redundant to specify config, as client already dereferences to config.

--- src/plugins/help.rs
+++ src/plugins/help.rs
@@ -5,7 +5,7 @@ fn help(event: &interface::CommandEvent) {
     let plugins = event.client.plugins.read();
     let commands = plugins.commands.keys().collect::<Vec<&String>>().connect(", ");
     event.client.send_message(event.channel, format!("Available commands: {}", commands).as_slice());
-    event.client.send_message(event.channel, format!("Use a command with `{}command_name` or `{}, command_name`", event.client.config.command_prefix, event.client.state.read().nick).as_slice());
+    event.client.send_message(event.channel, format!("Use a command with `{}command_name` or `{}, command_name`", event.client.command_prefix, event.client.state.read().nick).as_slice());
 }
 
 pub fn register(register: &mut client::PluginRegister) {
