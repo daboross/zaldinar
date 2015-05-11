d46358c561a9eaea74d341a37801eaad35e38337
Dabo Ross <daboross@daboross.net>
2015-05-11 02:33:48 -0700

    Workaround/fix for changes from https://github.com/rust-lang/rust/pull/25162

--- zaldinar-runtime/src/plugins/help.rs
+++ zaldinar-runtime/src/plugins/help.rs
@@ -4,13 +4,20 @@ use events::CommandEvent;
 fn help(event: &CommandEvent) {
     {
         let plugins = event.client.plugins().read().unwrap();
-        let commands = plugins.commands.keys().collect::<Vec<&String>>().connect(", ");
+        // TODO: `.map(|s| &**s)` is a workaround for changes made in
+        // https://github.com/rust-lang/rust/pull/25162 - it should be removed when (if) it
+        // becomes no longer required
+        let commands = plugins.commands.keys().map(|s| &**s).collect::<Vec<_>>().connect(", ");
 
         event.client.reply_notice(event, format!("Available commands: {}", commands));
         if event.client.is_mask_admin(event.mask()) {
             let plugins = event.client.plugins().read().unwrap();
-            let admin_commands = plugins.admin_commands.keys()
-                .collect::<Vec<&String>>().connect(", ");
+            // TODO: `.map(|s| &**s)` is a workaround for changes made in
+            // https://github.com/rust-lang/rust/pull/25162 - it should be removed when (if) it
+            // becomes no longer required
+            let admin_commands = plugins.admin_commands.keys().map(|s| &**s)
+                .collect::<Vec<_>>().connect(", ");
+
             event.client.reply_notice(event, format!("Admin commands: {}", admin_commands));
         }
     }
